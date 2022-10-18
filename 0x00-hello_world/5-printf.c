d3c8024b4b9fed078afefbb2fccd2a0aec232cb7
Ayo <ayomideaishatoke@gmail.com>
2022-10-17 20:09:13 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main - entry point for program to print code
  *
  * Return: 0
- */ 
+ */
 	int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art.\n");
-	return(0);
+	return (0);
 }
