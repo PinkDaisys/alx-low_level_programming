8b634901f800c415e276a6428d8e0570dfbf43b6
Ayo <ayomideaishatoke@gmail.com>
2022-10-23 02:31:03 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- program starting point
+ * Return: zero if no error
+ */
+int main(void)
+{
+	int i;
+	char hexvalues[] = "0123456789abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
