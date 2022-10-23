6f88dcd478c5b33d46278e1163d5f1bbbe8beff0
Ayo <ayomideaishatoke@gmail.com>
2022-10-23 01:54:24 -0700

    soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ * Return: zero if no error
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q'
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
