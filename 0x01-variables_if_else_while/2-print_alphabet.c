20798499bdaeeb958bf70d052cceccbcb6b5703e
Ayo <ayomideaishatoke@gmail.com>
2022-10-23 01:22:37 -0700

    sometimes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- program entry point
+ * Return: zero if no error
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
