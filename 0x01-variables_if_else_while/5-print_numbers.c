3df71a46269399dfa10f3795467f9ae0704a2d4d
Ayo <ayomideaishatoke@gmail.com>
2022-10-23 02:07:50 -0700

    soup

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- program starting point
+ * Return: zero of no error
+ *
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
