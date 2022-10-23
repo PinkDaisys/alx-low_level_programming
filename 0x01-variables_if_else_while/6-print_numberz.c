bddba3cb0a7b1e697927404e3dcd06df12e72df7
Ayo <ayomideaishatoke@gmail.com>
2022-10-23 02:15:58 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- program starting point
+ * Return: zero if no error
+ *
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
